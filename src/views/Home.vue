1372d3c6d312b3172ae72c2eaa95f8908ce3cb5a
Jorge <jorrillop@hotmail.com>
2020-04-06 16:17:17 +0200

    searchComment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,15 +8,33 @@
     <!-- <ul v-for="dato in datos" :key="dato.id">
       <li>{{ dato | tiempo(10) }}</li>
     </ul> -->
-    <Search name="star" :reviews="reviews"> </Search>
+    <!-- <Search name="star" :reviews="reviews"> </Search> -->
     <h3 class="text-left text-2xl pl-3 ml3 pb-5">
       Aquí puedes consultar las últimas valoraciones de los usuarios que han
       hecho sobre este producto
     </h3>
+
+    <div class="md:w-1/2 px-3">
+      <label
+        for="comment"
+        class="uppercase text-black text-base font-bold mb-2"
+      >
+        Busqueda por comentario de nuestros usuarios
+      </label>
+      <input
+        id="comment"
+        v-model="comment"
+        type="text"
+        class="w-full bg-gray-200
+      text-black border border-gray-200 rounded py-3 px-4 mb-3"
+        placeholder="Comentario usuario"
+      />
+    </div>
+
     <div class="flex flex-wrap w-full">
       <Card
         name="star"
-        v-for="review in reviews"
+        v-for="review in searchComment"
         :key="review.id"
         :review="review"
       >
@@ -26,28 +44,35 @@
 </template>
 <script>
 import Card from "@/components/Card.vue";
-import Search from "@/components/Search.vue";
+// import Search from "@/components/Search.vue";
 import db from "@/assets/db.json";
 export default {
   name: "home",
   components: {
-    Card,
-    Search
+    Card
+    // Search
   },
   data: function() {
     return {
       nombre: "jorge",
       isLoaded: false,
-      reviews: db.reviews
+      reviews: db.reviews,
+      comment: ""
     };
   },
+  methods: {},
   filters: {
     // tiempo: function(value, minutos) {
     //   if (value.tiempo < minutos) return value.nombre;
     // }
   },
-
-  created() {},
-  methods: {}
+  computed: {
+    searchComment() {
+      return this.reviews.filter(review =>
+        review.comment.includes(this.comment)
+      );
+    }
+  },
+  created() {}
 };
 </script>
