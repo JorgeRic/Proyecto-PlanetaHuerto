57a4791e56e92dc9cf3519e45fd063d1aa957d2d
Jorge <jorrillop@hotmail.com>
2020-04-06 18:45:02 +0200

    search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,6 @@
     >
       Valoraciones del producto
     </h1>
-    <!-- <ul v-for="dato in datos" :key="dato.id">
-      <li>{{ dato | tiempo(10) }}</li>
-    </ul> -->
-    <!-- <Search name="star" :reviews="reviews"> </Search> -->
     <h3 class="text-left text-2xl pl-3 ml3 pb-5">
       Aquí puedes consultar las últimas valoraciones de los usuarios que han
       hecho sobre este producto
@@ -24,6 +20,21 @@
       <input
         id="comment"
         v-model="comment"
+        :disabled="disabled1"
+        type="text"
+        class="w-full bg-gray-200
+      text-black border border-gray-200 rounded py-3 px-4 mb-3"
+        placeholder="Comentario usuario"
+      />
+    </div>
+    <div class="md:w-1/2 px-3">
+      <label for="author" class="uppercase text-black text-base font-bold mb-2">
+        Busqueda por usuario
+      </label>
+      <input
+        id="author"
+        v-model="author"
+        :disabled="disabled2"
         type="text"
         class="w-full bg-gray-200
       text-black border border-gray-200 rounded py-3 px-4 mb-3"
@@ -34,7 +45,7 @@
     <div class="flex flex-wrap w-full">
       <Card
         name="star"
-        v-for="review in searchComment"
+        v-for="review in search"
         :key="review.id"
         :review="review"
       >
@@ -44,33 +55,43 @@
 </template>
 <script>
 import Card from "@/components/Card.vue";
-// import Search from "@/components/Search.vue";
 import db from "@/assets/db.json";
 export default {
   name: "home",
   components: {
     Card
-    // Search
   },
   data: function() {
     return {
       nombre: "jorge",
       isLoaded: false,
       reviews: db.reviews,
-      comment: ""
+      comment: "",
+      author: "",
+      disabled1: false,
+      disabled2: false
     };
   },
   methods: {},
-  filters: {
-    // tiempo: function(value, minutos) {
-    //   if (value.tiempo < minutos) return value.nombre;
-    // }
-  },
+  filters: {},
   computed: {
-    searchComment() {
-      return this.reviews.filter(review =>
-        review.comment.includes(this.comment)
-      );
+    search() {
+      return this.reviews.filter(review => {
+        if (this.comment === "" && this.author === "") return review;
+        else {
+          if (this.comment !== "" && this.author === "") {
+            this.disabled2 = true;
+            return review.comment
+              .toLowerCase()
+              .includes(this.comment.toLowerCase());
+          } else if (this.comment === "" && this.author !== "") {
+            this.disabled1 = true;
+            return review.author
+              .toLowerCase()
+              .includes(this.author.toLowerCase());
+          }
+        }
+      });
     }
   },
   created() {}
